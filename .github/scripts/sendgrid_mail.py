eb1fd3320a51f15078042484fcab0983fd5c5779
Jurie Smit <smit.jurie@gmail.com>
2025-05-24 23:17:55 +0200

    Create sendgrid_mail.py

---
+++ .github/scripts/sendgrid_mail.py
@@ -0,0 +1,16 @@
+import os
+from sendgrid import SendGridAPIClient
+from sendgrid.helpers.mail import Mail
+
+sg = SendGridAPIClient(os.environ['SENDGRID_API_KEY'])
+message = Mail(
+    from_email='no-reply@yourdomain.com',
+    to_emails=os.environ['TO'],
+    subject=os.environ['SUBJECT'],
+    html_content=os.environ['BODY']
+)
+try:
+    response = sg.send(message)
+    print(f"Status: {response.status_code}")
+except Exception as e:
+    print(e)
